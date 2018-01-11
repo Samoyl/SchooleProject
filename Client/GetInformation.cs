b270699dc9f915dc04e57bd9036bc8f86635f906
Mohanad Yousif <mohanad-yousif@outlook.com>
2018-01-11 17:53:40 +0100

    Add project files.

---
+++ Client/GetInformation.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Views;
+using Android.Widget;
+
+namespace Client
+{
+    public class GetInformation : DialogFragment
+    {
+        public class OnGetInfoCompletEventArgs : EventArgs
+        {
+            public string TxtName { get; set; }
+            public int BackgroundColor { get; set; }
+        }
+
+        public event EventHandler<OnGetInfoCompletEventArgs> OnGetInfoComplete;
+
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            View view = inflater.Inflate(Resource.Layout.getinfo, container, false);
+
+            EditText txtName = view.FindViewById<EditText>(Resource.Id.txtName);
+            RadioGroup radioGroup = view.FindViewById<RadioGroup>(Resource.Id.radioGroup);
+            Button btnOkay = view.FindViewById<Button>(Resource.Id.btnOkay);            
+
+            btnOkay.Click += (o, e) =>
+            {
+                var args = new OnGetInfoCompletEventArgs { TxtName = txtName.Text.Trim() };
+
+                switch (radioGroup.CheckedRadioButtonId)
+                {
+                    case Resource.Id.radioRed:
+                        args.BackgroundColor = 1;
+                        break;
+
+                    case Resource.Id.radioGreen:
+                        args.BackgroundColor = 2;
+                        break;
+
+                    case Resource.Id.radioBlue:
+                        args.BackgroundColor = 3;
+                        break;
+                }
+
+                OnGetInfoComplete.Invoke(this, args);
+
+                Dismiss();
+            };
+
+            return view;
+        }
+
+        public override void OnActivityCreated(Bundle savedInstanceState)
+        {
+            Dialog.Window.RequestFeature(WindowFeatures.NoTitle);
+            base.OnActivityCreated(savedInstanceState);
+        }
+    }
+}
---
+++ Client/MainActivity.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using Android.App;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Microsoft.AspNet.SignalR.Client;
+using Android.Graphics;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+
+namespace Client
+{
+    [Activity(Label = "Client", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        public string UserName;
+        public int BackgroundColor;
+        public readonly String urlString = "http://signalr-samoyl.azurewebsites.net";
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            AppCenter.Start("34a69288-2d3b-4909-b542-188878f55b33",
+                   typeof(Analytics), typeof(Crashes));
+            GetInformation getInfo = new GetInformation();
+            getInfo.OnGetInfoComplete += GetInfo_OnGetInfoComplete;
+            getInfo.Show(FragmentManager, "GetInfo");
+        }
+
+        private async void GetInfo_OnGetInfoComplete(object sender, GetInformation.OnGetInfoCompletEventArgs e)
+        {
+            UserName = e.TxtName;
+            BackgroundColor = e.BackgroundColor;
+            var hubConnection = new HubConnection(urlString);
+            var chatHubProxy = hubConnection.CreateHubProxy("ChatHub");
+
+            chatHubProxy.On<string, int, string>("UpdateChatMessage", (message, color, user) =>
+            {
+                RunOnUiThread(() =>
+                {
+                    TextView txt = new TextView(this);
+                    txt.Text = user + ": " + message;
+                    txt.SetTextSize(Android.Util.ComplexUnitType.Sp, 20);
+                    txt.SetPadding(10, 10, 10, 10);
+
+                    switch (color)
+                    {
+                        case 1:
+                            txt.SetTextColor(Color.Red);
+                            break;
+
+                        case 2:
+                            txt.SetTextColor(Color.DarkGreen);
+                            break;
+
+                        case 3:
+                            txt.SetTextColor(Color.Blue);
+                            break;
+
+                        default:
+                            txt.SetTextColor(Color.Black);
+                            break;
+
+                    }
+
+                    txt.LayoutParameters = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WrapContent, ViewGroup.LayoutParams.WrapContent)
+                    {
+                        TopMargin = 10,
+                        BottomMargin = 10,
+                        LeftMargin = 10,
+                        RightMargin = 10,
+                        Gravity = GravityFlags.Left
+                    };
+
+                    FindViewById<LinearLayout>(Resource.Id.llChatMessages)
+                            .AddView(txt);
+                });
+            });
+
+            try
+            {
+                await hubConnection.Start();
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            FindViewById<Button>(Resource.Id.btnSend).Click += async (o, e2) =>
+            {
+                var message = FindViewById<EditText>(Resource.Id.txtChat).Text;
+                FindViewById<EditText>(Resource.Id.txtChat).Text = null;
+                await chatHubProxy.Invoke("SendMessage", new object[] { message, BackgroundColor, UserName });
+            };
+
+        }
+    }
+}
