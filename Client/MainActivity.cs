5bd32d4e38c3e87d759f5336a6d989c0eaedec8c
Mohanad Yousif <mohanad-yousif@outlook.com>
2018-01-11 18:39:35 +0100

    just for test

--- Client/MainActivity.cs
+++ Client/MainActivity.cs
@@ -23,7 +23,6 @@ namespace Client
             
             base.OnCreate(savedInstanceState);
 
-            // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
             AppCenter.Start("34a69288-2d3b-4909-b542-188878f55b33",
                    typeof(Analytics), typeof(Crashes));
